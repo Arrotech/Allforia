87291bb9e493dd20c9cb77b26ff2c067ac15a601
Arrotech <arrotechdesign@gmail.com>
2019-09-24 02:55:07 +0300

    Chore (Nav Menu) Add navigation menu
    What does this pr do?
    
    Add a navigation menu to the homepage.
    
    Any related pivotal stoies?
    
    [Finishes #168714121]

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+      <div className="nav">
+          <input type="checkbox" id="nav-check"/>
+          <div className="nav-btn">
+            <label for="nav-check">
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+          <div className="nav-links">
+            <a href="#allforia">Allforia</a>
+            <a href="#services">Services</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+            <a href="#register">Register</a>
+            <a href="#login">Login</a>
+          </div>
+      </div>
+      </div>
+      );
+    }
+    export default App;
